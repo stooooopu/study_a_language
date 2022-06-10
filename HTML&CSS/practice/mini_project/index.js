b6bb8163cc9c1ac52d25abae37329fe3d6be0079
stooooopu <jyg5193@naver.com>
2022-06-10 16:23:02 +0900

    feat js파일 추가

---
+++ HTML&CSS/practice/mini_project/index.js
@@ -0,0 +1,11 @@
+$('.prolog-btn').click(function(){
+    // console.log('prolog button')
+    $('.main-prolog').css('display','block')
+    $('.main-blog').css('display','none')
+})
+
+$('.blog-btn').click(function(){
+    // console.log('prolog button')
+    $('.main-prolog').css('display','none')
+    $('.main-blog').css('display','block')
+})
